379a9a60c720451248d57b31a0d3f87118fd35cb
soroush2025 <soroush2025@gmail.com>
2025-02-23 16:53:06 +0330

    fix adapter for local

--- packages/prisma/edge.ts
+++ packages/prisma/edge.ts
@@ -1,12 +1,19 @@
 import { Client } from "@planetscale/database";
 import { PrismaPlanetScale } from "@prisma/adapter-planetscale";
 import { PrismaClient } from "@prisma/client";
+import { Result_4, Transaction } from "@prisma/client/runtime/library";
 
 const client = new Client({
   url: process.env.PLANETSCALE_DATABASE_URL || process.env.DATABASE_URL,
 });
+class CustomPrismaPlanetScale extends PrismaPlanetScale {
+  async startTransaction(): Promise<Result_4<Transaction>> {
+    // Implement the startTransaction method here
+    throw new Error("Method not implemented.");
+  }
+}
 
-const adapter = new PrismaPlanetScale(client);
+const adapter = new CustomPrismaPlanetScale(client);
 
 export const prismaEdge = new PrismaClient({
   adapter,
